246c91747ba0d411caea4b02dbb56eb1d434c149
AhadAli01 <ahadali101aa@gmail.com>
2023-02-08 16:21:27 -0700

    Added test cases for Range.getLength()

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -20,11 +20,11 @@ public class RangeTest {
 		exampleRange = new Range(-1, 1);
 	}
 
-	@Test
-	public void centralValueShouldBeZero() {
-		assertEquals("The central value of -1 and 1 should be 0",
-				0, exampleRange.getCentralValue(), .000000001d);
-	}
+//	@Test
+//	public void centralValueShouldBeZero() {
+//		assertEquals("The central value of -1 and 1 should be 0",
+//				0, exampleRange.getCentralValue(), .000000001d);
+//	}
 	// ! End of Code provided by Lab Doc.
 
 	// Testing getLength()
@@ -39,6 +39,66 @@ public class RangeTest {
 		assertEquals("Length should be 0", expectedLength, actualLength, .000000001d);
 	}
 
+	// Test the length of a range with positive upper and lower bounds
+	@Test
+	public void testGetLengthBothBoundsPositive() {
+		Range sampleRange = new Range(0, 24);
+
+		double expectedLength = 24 - 0;
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 24.0", expectedLength, actualLength, .000000001d);
+	}
+
+	// Test the length of a range with negative upper and lower bounds
+	@Test
+	public void testGetLengthBothBoundsNegative() {
+		Range sampleRange = new Range(-100, -24);
+
+		double expectedLength = -24 - (-100);
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 76.0", expectedLength, actualLength, .000000001d);
+	}
+
+	// Test the length of a range with negative lower and positive upper bounds
+	@Test
+	public void testGetLengthOnlyLowerBoundNegative() {
+		Range sampleRange = new Range(-100, 100);
+
+		double expectedLength = 100 - (-100);
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 200.0", expectedLength, actualLength, .000000001d);
+	}
+
+	// Test the length of a range with a decimal upper and lower bound
+	@Test
+	public void testGetLengthBothBoundsDecimals() {
+		Range sampleRange = new Range(-123.24, 100.24);
+
+		double expectedLength = 100.24 - (-123.24);
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 223.48", expectedLength, actualLength, .000000001d);
+	}
+
+	// Test the length of a range with a decimal upper and whole number lower bound
+	@Test
+	public void testGetLengthOnlyUpperBoundDecimal() {
+		Range sampleRange = new Range(-123, 100.24);
+
+		double expectedLength = 100.24 - (-123);
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 223.24", expectedLength, actualLength, .000000001d);
+	}
+
+	// Test the length of a range with a decimal lower and whole number upper bound
+	@Test
+	public void testGetLengthOnlyLowerBoundDecimal() {
+		Range sampleRange = new Range(-123.24, 150);
+
+		double expectedLength = 150 - (-123.24);
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 273.24", expectedLength, actualLength, .000000001d);
+	}
+	
 	// ! Provided by SENG 438 Lab Document
 	@After
 	public void tearDown() throws Exception {

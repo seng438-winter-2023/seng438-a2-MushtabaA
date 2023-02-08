0200e4bc44c693bc96f75f6340c666304b22cc61
AhadAli01 <ahadali101aa@gmail.com>
2023-02-08 16:02:44 -0700

    Try one test

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -1,29 +1,51 @@
 package org.jfree.data.test;
 
-import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.*;
+import static org.junit.Assert.*;
+import org.jfree.data.Range;
+import org.junit.Test;
+import org.junit.*;
+
 
 public class RangeTest {
-    private Range exampleRange;
-    @BeforeClass public static void setUpBeforeClass() throws Exception {
-    }
 
+	private Range exampleRange;
 
-    @Before
-    public void setUp() throws Exception { exampleRange = new Range(-1, 1);
-    }
+	// ! Provided by SENG 438 Lab Document
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
 
+	@Before
+	public void setUp() throws Exception {
+		exampleRange = new Range(-1, 1);
+	}
 
-    @Test
-    public void centralValueShouldBeZero() {
-        assertEquals("The central value of -1 and 1 should be 0",
-        0, exampleRange.getCentralValue(), .000000001d);
-    }
+	@Test
+	public void centralValueShouldBeZero() {
+		assertEquals("The central value of -1 and 1 should be 0",
+				0, exampleRange.getCentralValue(), .000000001d);
+	}
+	// ! End of Code provided by Lab Doc.
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	// Testing getLength()
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
+	// Test the length of a range that is 0.0
+	@Test
+	public void testGetLengthZero() {
+		Range sampleRange = new Range(24, 24);
+
+		double expectedLength = 0;
+		double actualLength = sampleRange.getLength();
+		assertEquals("Length should be 0", expectedLength, actualLength, .000000001d);
+	}
+
+	// ! Provided by SENG 438 Lab Document
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	// ! End of Code provided by Lab Doc.
 }

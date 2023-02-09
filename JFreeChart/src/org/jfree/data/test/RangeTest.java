686c4d02ed110b9a5bac9af4d555529867d18f33
AhadAli01 <ahadali101aa@gmail.com>
2023-02-08 17:24:02 -0700

    Added Range.contains test cases

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -99,6 +99,69 @@ public class RangeTest {
 		assertEquals("Length should be 273.24", expectedLength, actualLength, .000000001d);
 	}
 	
+	// Testing contains()
+
+		// Test if a valid integer exists in range
+		@Test
+		public void testGetContainsNumberExists() {
+			Range sampleRange = new Range(20, 24);
+
+			boolean expectedContains = true;
+			boolean actualContains = sampleRange.contains(23);
+			assertEquals("22 should exist in range of 20 to 24", expectedContains, actualContains);
+		}
+
+		// Test if a valid integer does not exist in range
+		@Test
+		public void testGetContainsNumberNotExists() {
+			Range sampleRange = new Range(20, 24);
+
+			boolean expectedContains = false;
+			boolean actualContains = sampleRange.contains(25);
+			assertEquals("25 should not exist in range of 20 to 24", expectedContains, actualContains);
+		}
+		
+		// Test if a valid integer does exist in range and is lower bound
+		@Test
+		public void testGetContainsPositiveNumberExistsLowerBound() {
+			Range sampleRange = new Range(20, 24);
+
+			boolean expectedContains = true;
+			boolean actualContains = sampleRange.contains(20);
+			assertEquals("20 should exist in range of -20 to 24", expectedContains, actualContains);
+		}
+		
+		// Test if a valid integer does exist in range and is upper bound
+		@Test
+		public void testGetContainsPositiveNumberExistsUpperBound() {
+			Range sampleRange = new Range(42, 224);
+
+			boolean expectedContains = true;
+			boolean actualContains = sampleRange.contains(224);
+			assertEquals("224 should exist in range of 42 to 224", expectedContains, actualContains);
+		}
+		// Test if a valid integer does exist in range and is lower bound and is negative
+		@Test
+		public void testGetContainsNegativeNumberExistsLowerBound() {
+			Range sampleRange = new Range(-20, 24);
+
+			boolean expectedContains = true;
+			boolean actualContains = sampleRange.contains(-20);
+			assertEquals("-20 should exist in range of -20 to 24", expectedContains, actualContains);
+		}
+		
+		// Test if a valid integer does exist in range and is upper bound and is negative
+		@Test
+		public void testGetContainsNegativeNumberExistsUpperBound() {
+			Range sampleRange = new Range(-42, -24);
+
+			boolean expectedContains = true;
+			boolean actualContains = sampleRange.contains(-24);
+			assertEquals("-24 should exist in range of -42 to -24", expectedContains, actualContains);
+		}
+		
+		
+	
 	// ! Provided by SENG 438 Lab Document
 	@After
 	public void tearDown() throws Exception {

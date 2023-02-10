c412d460439c4c68545e0b32e934d663dfa194d9
AhadAli01 <ahadali101aa@gmail.com>
2023-02-09 18:56:07 -0700

    Add DataUtilities.calculateColumnTotal test cases

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -6,15 +6,19 @@ import org.jfree.data.DataUtilities;
 import org.jfree.data.Values2D;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.api.ExpectationError;
 import org.junit.Test;
 
 public class DataUtilitiesTest extends DataUtilities {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 	
+	 // Testing calculateColumnTotal(Values2D data, int column)
+	
+	// Sample Test Case provided by SENG 438 Lab Document
 	 @Test
 	 public void calculateColumnTotalForTwoValues() {
 	     // setup
@@ -36,5 +40,113 @@ public class DataUtilitiesTest extends DataUtilities {
 	     assertEquals(result, 10.0, .000000001d);
 	     // tear-down: NONE in this test method
 	 }
+	 
+	 // Test a null data parameter which is invalid
+	 @Test(expected = NullPointerException.class)
+		public void calculateColumnTotalNullData() {
+			double actualColumnTotal = DataUtilities.calculateColumnTotal(null, 0);
+		}
+	 
+	 // Test an empty data parameter
+	 @Test
+		public void calculateColumnTotalEmpty() {
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
 
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getRowCount();
+					will(returnValue(0));
+				}
+			});
+			
+			double expectedColumnTotal = 0;
+			double actualColumnTotal = DataUtilities.calculateColumnTotal(values, 0);
+			assertEquals("The column total for empty object should be 0", expectedColumnTotal, actualColumnTotal, .000000001d);
+		}
+	 
+	 // Test a valid data parameter with positive values and positive index and other values in other columns
+	 @Test
+		public void calculateColumnTotalPositiveTableAndValues() {
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getRowCount();
+					will(returnValue(3));
+
+					one(values).getValue(0, 0);
+					will(returnValue(1000));
+					one(values).getValue(0, 1);
+					will(returnValue(14));
+					one(values).getValue(0, 2);
+					will(returnValue(1000));
+
+					one(values).getValue(1, 1);
+					will(returnValue(24));
+
+					one(values).getValue(2, 1);
+					will(returnValue(34));
+				}
+			});
+			
+			double expectedColumnTotal = 72;
+			double actualColumnTotal = DataUtilities.calculateColumnTotal(values, 1);
+			assertEquals("The column total should be 72", expectedColumnTotal, actualColumnTotal, .000000001d);
+		}
+	 
+	 // Test a valid data parameter with positive values and positive index and other values in other columns
+	 @Test
+		public void calculateColumnTotalNegativeValues() {
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getRowCount();
+					will(returnValue(3));
+
+					one(values).getValue(0, 0);
+					will(returnValue(-1000));
+					one(values).getValue(0, 1);
+					will(returnValue(-14));
+					one(values).getValue(0, 2);
+					will(returnValue(-1000));
+
+					one(values).getValue(1, 1);
+					will(returnValue(-24));
+
+					one(values).getValue(2, 1);
+					will(returnValue(-34));
+				}
+			});
+			
+			double expectedColumnTotal = -72;
+			double actualColumnTotal = DataUtilities.calculateColumnTotal(values, 1);
+			assertEquals("The column total should be -72", expectedColumnTotal, actualColumnTotal, .000000001d);
+		}
+	 
+
+	 
+	 // Test a valid data parameter with positive values and negative index
+		@Test(expected = ExpectationError.class)
+		public void calculateColumnTotalNegativeColumn() {
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getRowCount();
+					will(returnValue(3));
+
+					one(values).getValue(0, 0);
+					will(returnValue(1000));
+				}
+			});
+			
+			double expectedColumnTotal = 72;
+			double actualColumnTotal = DataUtilities.calculateColumnTotal(values, -1);
+		}
+	 
 }
